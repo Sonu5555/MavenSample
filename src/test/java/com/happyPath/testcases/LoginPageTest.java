5311f23ecef8924ed8cd0d2004354c5a44551e7f
Sonu5555 <sonu@socxo.com>
2021-04-06 12:49:54 +0530

    Git push2

--- src/test/java/com/happyPath/testcases/LoginPageTest.java
+++ src/test/java/com/happyPath/testcases/LoginPageTest.java
@@ -3,6 +3,7 @@ package com.happyPath.testcases;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import com.happyPath.base.TestBase;
 import com.happyPath.config.PropertyManager;
@@ -27,13 +28,11 @@ public class LoginPageTest extends TestBase{
 	public void LoginSuccessTest() {
 		loginPage.login(mgr.username(),mgr.passwrd());
 		 System.out.println("@Test "+mgr.username());
-		
+		 SoftAssert softassert = new SoftAssert();
+		 softassert.assertEquals(mgr.username(), "sonu@socxo.com");
+		 softassert.assertAll();
 	}
-	@Test(priority = 1)
-	public void testMesssage() {
-		loginPage.login(mgr.username(),mgr.passwrd());
-		 System.out.println("@Test "+mgr.username());
-	}
+
 	@AfterMethod
 	public void tearDown() {
 		driver.quit();
